688feac67142b59d66b3d637d486cb79e8cf8b3d
olandr <sonomis1@gmail.com>
2020-03-09 20:18:03 +0100

    Reworked a game-case such that has binary voting in 4 districts.

--- main.py
+++ main.py
@@ -8,33 +8,28 @@ L = [0, 1, 2, 10, 11, 12, 20, 21, 22]
 #initial state
 L0 = 0
 #action alphabet
-Sigma = (("0a", "1a", "2a"), ("0b", "1b", "2b"))
+Sigma = (("0a", "1a"), ("0b", "1b"))
 #action labeled transitions
 Delta = [
     (0, ("0a", "0b"),   0),
     (0, ("0a", "1b"),   1),
     (0, ("1a", "0b"),  10),
     (0, ("1a", "1b"),  11),
+
+    (1, ("0a", "0b"),   1),
+    (1, ("0a", "1b"),   2),
     (1, ("1a", "0b"),  11),
-    (10, ("0a", "1b"), 11),
-    (0, ("2a", "0b"),  20),
-    (0, ("2a", "1b"),  21),
-    (0, ("2a", "2b"),  22),
-    (0, ("0a", "2b"),   2),
-    (0, ("1a", "2b"),  12),
-    (1, ("2a", "0b"),  21),
-    (1, ("2a", "1b"),  22),
-    (2, ("1a", "0b"),  12),
-    (2, ("2a", "0b"),  22),
-    (0, ("0a", "2b"),   2),
-    (10, ("0a", "2b"), 12),
-    (10, ("1a", "2b"), 22),
-    (10, ("0a", "2b"), 12),
-    (11, ("1a", "1b"), 22),
-    (11, ("0a", "1b"), 12),
-    (11, ("1a", "0b"), 21)
+    (1, ("1a", "1b"),  12),
 
+    (10, ("0a", "0b"),  10),
+    (10, ("0a", "1b"),  11),
+    (10, ("1a", "0b"),  20),
+    (10, ("1a", "1b"),  21),
 
+    (11, ("0a", "0b"),  11),
+    (11, ("0a", "1b"),  12),
+    (11, ("1a", "0b"),  21),
+    (11, ("1a", "1b"),  22)
 
 ]
 #observation partitioning
@@ -46,8 +41,10 @@ Obs = [
 #G is a MultiplayerGame-object, and so are GK and GK0
 G = MultiplayerGame.create(L, L0, Sigma, Delta, Obs)
 GK = G.KBSC()
-GK0 = GK.project(0)
+
+(log, GK2, iso_type) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
+
 
 #export the GK game to ./pictures/GK.png
 export(GK, "GK")
-export(GK0, "GK0")
+export(GK2, "GK2")
