70f6a1d607e8aca2a516b941b9232a04a5f3bf35
olandr <sonomis1@gmail.com>
2020-03-09 20:32:20 +0100

    This is a game that we might be happy with? Because this game is in essence giving a draw, a-lead, b-lead, a-win or b-win. We might be able to use this as an abstraction on whether voter A or B drew, lost or won some district.

--- main.py
+++ main.py
@@ -4,47 +4,40 @@ from mkbsc import MultiplayerGame, iterate_until_isomorphic, \
                   export, to_string, from_string, to_file, from_file
 
 #states
-L = [0, 1, 2, 10, 11, 12, 20, 21, 22]
+L = ["start", "draw", "Alead", "Blead", "Awin", "Bwin"]
 #initial state
-L0 = 0
+L0 = "start"
 #action alphabet
 Sigma = (("0a", "1a"), ("0b", "1b"))
 #action labeled transitions
 Delta = [
-    (0, ("0a", "0b"),   0),
-    (0, ("0a", "1b"),   1),
-    (0, ("1a", "0b"),  10),
-    (0, ("1a", "1b"),  11),
+    ("start", ("0a", "0b"), "draw"),
+    ("start", ("0a", "1b"),  "Blead"),
+    ("start", ("1a", "0b"),  "Alead"),
 
-    (1, ("0a", "0b"),   1),
-    (1, ("0a", "1b"),   2),
-    (1, ("1a", "0b"),  11),
-    (1, ("1a", "1b"),  12),
+    ("draw", ("0a", "0b"), "draw"),
+    ("draw", ("0a", "1b"), "Blead"),
+    ("draw", ("1a", "0b"),  "Alead"),
 
-    (10, ("0a", "0b"),  10),
-    (10, ("0a", "1b"),  11),
-    (10, ("1a", "0b"),  20),
-    (10, ("1a", "1b"),  21),
+    ("Alead", ("0a", "0b"), "Alead"),
+    ("Alead", ("0a", "1b"), "draw"),
+    ("Alead", ("1a", "0b"), "Awin"),
 
-    (11, ("0a", "0b"),  11),
-    (11, ("0a", "1b"),  12),
-    (11, ("1a", "0b"),  21),
-    (11, ("1a", "1b"),  22)
+    ("Blead", ("0a", "0b"), "Blead"),
+    ("Blead", ("1a", "0b"),  "draw"),
+    ("Blead", ("0a", "1b"), "Bwin")
 
 ]
 #observation partitioning
 Obs = [
-    [[0,  1, 2], [10, 11, 12], [20, 21, 22]],
-    [[0, 10, 20], [1, 11, 21], [2, 12, 22]]
+    [["start"], ["draw", "Alead", "Blead"], ["Awin"], ["Bwin"]],
+    [["start"], ["draw", "Alead", "Blead"], ["Awin"], ["Bwin"]]
 ]
 
 #G is a MultiplayerGame-object, and so are GK and GK0
 G = MultiplayerGame.create(L, L0, Sigma, Delta, Obs)
 GK = G.KBSC()
-
-(log, GK2, iso_type) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
-
-
-#export the GK game to ./pictures/GK.png
 export(GK, "GK")
-export(GK2, "GK2")
+
+#(log, GK2, iso_type) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
+#export(GK2, "GK2")
