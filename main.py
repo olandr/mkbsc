fd73ad44c4d8b8f6231a5d30d313a398b75f87f8
olandr <sonomis1@gmail.com>
2020-03-09 16:56:34 +0100

    Added initial gamecase

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ from mkbsc import MultiplayerGame, iterate_until_isomorphic, \
                   export, to_string, from_string, to_file, from_file
 
 #states
-L = [0, 1, 2]
+L = [0, 1, 10, 11, 12, 21]
 #initial state
 L0 = 0
 #action alphabet
-Sigma = (("w", "p"), ("w", "p"))
+Sigma = (("0a", "1a"), ("0b", "1b"))
 #action labeled transitions
 Delta = [
-    (0, ("p", "p"), 0), (0, ("w", "w"), 0),
-    (0, ("w", "p"), 1), (0, ("p", "w"), 2),
-    (1, ("p", "p"), 1), (1, ("w", "w"), 1),
-    (1, ("w", "p"), 2), (1, ("p", "w"), 0),
-    (2, ("p", "p"), 2), (2, ("w", "w"), 2),
-    (2, ("w", "p"), 0), (2, ("p", "w"), 1)
+    (0, ("0a", "0b"),   0),
+    (0, ("0a", "1b"),   1),
+    (0, ("1a", "0b"),  10),
+    (0, ("1a", "1b"),  11),
+    (1, ("1a", "0b"),  11),
+    (10, ("0a", "1b"), 11)
+
 ]
 #observation partitioning
 Obs = [
-    [[0, 1], [2]],
-    [[0, 2], [1]]
+    [[0,  1], [10, 11, 12], [21]],
+    [[0, 10], [1, 11, 21], [12]]
 ]
 
 #G is a MultiplayerGame-object, and so are GK and GK0
@@ -31,3 +32,4 @@ GK0 = GK.project(0)
 
 #export the GK game to ./pictures/GK.png
 export(GK, "GK")
+export(GK0, "GK0")
