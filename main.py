ec954dc27aa0a2e93765d9e55f969a8695b921ca
olandr <sonomis1@gmail.com>
2020-03-10 10:01:22 +0100

    added weighed distribution model. it looks kinda trivial

--- main.py
+++ main.py
@@ -4,65 +4,60 @@ from mkbsc import MultiplayerGame, iterate_until_isomorphic, \
                   export, to_string, from_string, to_file, from_file
 
 #states
-L = ["draw", "a", "b", "ab", "aa", "bb", "aab", "abb", "A", "B"]
+L = ["start", "draw", "0", "1", "a", "b", "aab", "abb", "A", "B"]
 #initial state
-L0 = "draw"
+L0 = "start"
 #action alphabet
 Sigma = (("0a", "1a"), ("0b", "1b"))
 #action labeled transitions
 Delta = [
-    ("draw", ("0a", "0b"),  "draw"),
-    ("draw", ("0a", "1b"),  "b"),
-    ("draw", ("1a", "0b"),  "a"),
-    ("draw", ("1a", "1b"),  "draw"),
+    ("start", ("0a", "0b"),  "0"),
+    ("start", ("0a", "1b"),  "b"),
+    ("start", ("1a", "0b"),  "a"),
+    ("start", ("1a", "1b"),  "0"),
 
-    ("a", ("0a", "0b"),  "a"),
-    ("a", ("0a", "1b"),  "draw"),
-    ("a", ("1a", "0b"),  "aa"),
-    ("a", ("1a", "1b"),  "aab"),
+    ("0", ("0a", "0b"),  "1"),
+    ("0", ("0a", "1b"),  "B"),
+    ("0", ("1a", "0b"),  "A"),
+    ("0", ("1a", "1b"),  "1"),
 
-    ("b", ("0a", "0b"),  "b"),
-    ("b", ("0a", "1b"),  "bb"),
-    ("b", ("1a", "0b"),  "draw"),
-    ("b", ("1a", "1b"),  "abb"),
+    ("a", ("0a", "0b"),  "1"),
+    ("a", ("0a", "1b"),  "abb"),
+    ("a", ("1a", "0b"),  "A"),
+    ("a", ("1a", "1b"),  "A"),
 
-    ("ab", ("0a", "0b"),  "draw"),
-    ("ab", ("0a", "1b"),  "abb"),
-    ("ab", ("1a", "0b"),  "aab"),
-    ("ab", ("1a", "1b"),  "draw"),
+    ("b", ("0a", "0b"),  "1"),
+    ("b", ("0a", "1b"),  "B"),
+    ("b", ("1a", "0b"),  "aab"),
+    ("b", ("1a", "1b"),  "B"),
 
-    ("aa", ("0a", "0b"),  "aa"),
-    ("aa", ("0a", "1b"),  "aab"),
-    ("aa", ("1a", "0b"),  "A"),
-    ("aa", ("1a", "1b"),  "A"),
-
-    ("bb", ("0a", "0b"),  "bb"),
-    ("bb", ("0a", "1b"),  "B"),
-    ("bb", ("1a", "0b"),  "abb"),
-    ("bb", ("1a", "1b"),  "B"),
-
-    ("aab", ("0a", "0b"),  "A"),
-    ("aab", ("0a", "1b"),  "draw"),
-    ("aab", ("1a", "0b"),  "A"),
-    ("aab", ("1a", "1b"),  "A"),
+    ("1", ("0a", "0b"),  "draw"),
+    ("1", ("0a", "1b"),  "B"),
+    ("1", ("1a", "0b"),  "A"),
+    ("1", ("1a", "1b"),  "draw"),
 
     ("abb", ("0a", "0b"),  "B"),
     ("abb", ("0a", "1b"),  "B"),
     ("abb", ("1a", "0b"),  "draw"),
     ("abb", ("1a", "1b"),  "B"),
 
+    ("aab", ("0a", "0b"),  "A"),
+    ("aab", ("0a", "1b"),  "draw"),
+    ("aab", ("1a", "0b"),  "A"),
+    ("aab", ("1a", "1b"),  "A")
+
 ]
 #observation partitioning
 Obs = [
-    [["draw"], ["b", "bb"], ["a", "ab", "abb"], ["aa", "aab"], ["A"], ["B"]],
-    [["draw"], ["a", "aa"], ["b", "ab", "aab"], ["bb", "abb"], ["A"], ["B"]]
+    [["start"], ["draw", "0", "1"], ["b"], ["a", "abb"], ["aab"], ["A"], ["B"]],
+    [["start"], ["draw", "0", "1"], ["a"], ["b", "aab"], ["abb"], ["A"], ["B"]]
 ]
 
 #G is a MultiplayerGame-object, and so are GK and GK0
 G = MultiplayerGame.create(L, L0, Sigma, Delta, Obs)
 
-#GK = G.KBSC()
-#export(GK, "GK")
+GK = G.KBSC()
+export(GK, "GK")
 
-(log2, GK2, iso_type2) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
-export(GK2, "GK2")
+#(log2, GK2, iso_type2) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
+#export(GK2, "GK2")
