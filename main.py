fa92bb6548f7f4eb2bceadecfb8ad07da9d83ba4
olandr <sonomis1@gmail.com>
2020-03-10 09:25:46 +0100

    Not sure what I have here. But another kind of game, this is actually isomoprhic for G2k

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from mkbsc import MultiplayerGame, iterate_until_isomorphic, \
                   export, to_string, from_string, to_file, from_file
 
 #states
-L = ["draw", "a", "b", "ab", "aa", "bb", "aab", "aabb", "abb", "A", "B"]
+L = ["draw", "a", "b", "ab", "aa", "bb", "aab", "abb", "A", "B"]
 #initial state
 L0 = "draw"
 #action alphabet
@@ -14,22 +14,22 @@ Delta = [
     ("draw", ("0a", "0b"),  "draw"),
     ("draw", ("0a", "1b"),  "b"),
     ("draw", ("1a", "0b"),  "a"),
-    ("draw", ("1a", "1b"),  "ab"),
+    ("draw", ("1a", "1b"),  "draw"),
 
     ("a", ("0a", "0b"),  "a"),
-    ("a", ("0a", "1b"),  "ab"),
+    ("a", ("0a", "1b"),  "draw"),
     ("a", ("1a", "0b"),  "aa"),
     ("a", ("1a", "1b"),  "aab"),
 
     ("b", ("0a", "0b"),  "b"),
     ("b", ("0a", "1b"),  "bb"),
-    ("b", ("1a", "0b"),  "ab"),
+    ("b", ("1a", "0b"),  "draw"),
     ("b", ("1a", "1b"),  "abb"),
 
-    ("ab", ("0a", "0b"),  "ab"),
+    ("ab", ("0a", "0b"),  "draw"),
     ("ab", ("0a", "1b"),  "abb"),
     ("ab", ("1a", "0b"),  "aab"),
-    ("ab", ("1a", "1b"),  "aabb"),
+    ("ab", ("1a", "1b"),  "draw"),
 
     ("aa", ("0a", "0b"),  "aa"),
     ("aa", ("0a", "1b"),  "aab"),
@@ -51,22 +51,18 @@ Delta = [
     ("abb", ("1a", "0b"),  "draw"),
     ("abb", ("1a", "1b"),  "B"),
 
-    ("aabb", ("0a", "0b"),  "draw"),
-    ("aabb", ("0a", "1b"),  "B"),
-    ("aabb", ("1a", "0b"),  "A"),
-    ("aabb", ("1a", "1b"),  "draw")
 ]
 #observation partitioning
 Obs = [
-    [["draw", "b", "bb"], ["a", "ab", "abb"], ["aa", "aab", "aabb"], ["A"], ["B"]],
-    [["draw", "a", "aa"], ["b", "ab", "aab"], ["bb", "abb", "aabb"], ["A"], ["B"]]
+    [["draw"], ["b", "bb"], ["a", "ab", "abb"], ["aa", "aab"], ["A"], ["B"]],
+    [["draw"], ["a", "aa"], ["b", "ab", "aab"], ["bb", "abb"], ["A"], ["B"]]
 ]
 
 #G is a MultiplayerGame-object, and so are GK and GK0
 G = MultiplayerGame.create(L, L0, Sigma, Delta, Obs)
 
-GK = G.KBSC()
-export(GK, "GK")
+#GK = G.KBSC()
+#export(GK, "GK")
 
-(log, GK2, iso_type) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
+(log2, GK2, iso_type2) = iterate_until_isomorphic(G, limit=2, print_size=False, verbose=True)
 export(GK2, "GK2")
